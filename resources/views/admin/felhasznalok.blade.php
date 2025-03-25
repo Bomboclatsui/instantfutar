27f6d9f53140e2ba9bfc42de6da48253847ee741
Bablina Balázs <bablinabalazs@gmail.com>
2025-03-25 12:03:14 +0000

    admin/user

--- resources/views/admin/felhasznalok.blade.php
+++ resources/views/admin/felhasznalok.blade.php
@@ -10,7 +10,7 @@
         <title>Felhasználok Adatbázis Táblázat</title>
         <!-- Latest compiled and minified CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
+        <link rel="stylesheet" href="{{asset('css/style_admin.css')}}">
         <!-- Latest compiled JavaScript -->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     </head>
@@ -52,15 +52,12 @@
                                 <form action="{{ route('felhasznalo.modosit') }}" method="POST">
                                     @csrf
                                     <input type="hidden" name="id" value="{{ $felhasznalo->id }}">
-                                    <button type="submit" class="btn btn-warning btn-sm">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                            fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
-                                            <path
-                                                d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                            <path fill-rule="evenodd"
-                                                d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z" />
-                                        </svg>
-                                    </button>
+                                    <label class="switch">
+                                        <input type="checkbox" @if ($felhasznalo->tipus == 'admin')
+                                            checked
+                                        @endif onchange="this.form.submit()">
+                                        <span class="slider"></span>
+                                      </label>
                                 </form>
                             </td>
                         </tr>
